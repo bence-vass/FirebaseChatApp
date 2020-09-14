92a3cff41ded8aa43ccaf1aa78d150d791a06a4c
Bence Vass <v.bence.vass@gmail.com>
2020-09-14 12:47:54 +0200

    Updates

---
+++ precache-manifest.c55bcd1742c0955245e9aa180ab75555.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e703ad4d5f2e8469a421ca4af522ac7",
+    "url": "/FirebaseChatApp/index.html"
+  },
+  {
+    "revision": "cef583b551a808f5141f",
+    "url": "/FirebaseChatApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "173dfe664dd639622445",
+    "url": "/FirebaseChatApp/static/js/2.7ca51148.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/FirebaseChatApp/static/js/2.7ca51148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cef583b551a808f5141f",
+    "url": "/FirebaseChatApp/static/js/main.f33a08a5.chunk.js"
+  },
+  {
+    "revision": "625997309301cd2504eb",
+    "url": "/FirebaseChatApp/static/js/runtime-main.ba671e2b.js"
+  }
+]);
